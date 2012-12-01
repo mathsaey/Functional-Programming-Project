efa2667d16aaaa84a5b737fde838309dce2fcedd
Mathijs Saey <mathsaey@Greyflood.local>
2012-12-01 16:53:16 +0100

    Finished implementing the Dijkstra algorithm, gave it it's own module

---
+++ Graph/Dijkstra.hs
@@ -0,0 +1,82 @@
+-- Dijkstra.hs
+-- Mathijs Saey
+-- This module contains the implementation of the graph algorithms
+
+module Graph.Dijkstra (dijkstra) where
+
+import Graph.Kernel
+import Data.Maybe
+import Data.List
+
+-- Provides an abstraction to deal with
+-- infinity, regardless of the data type
+data Inf e = INF | NI e deriving (Read, Show, Eq)
+
+instance (Ord e) => Ord (Inf e) where
+	compare (NI x) (NI y) = compare x y 
+	compare INF INF = EQ
+	compare INF _ = GT
+	compare _ INF = LT
+
+front :: (a,b,c) -> a
+front (x,_,_) = x 
+
+center :: (a,b,c) -> b
+center (_,x,_) = x 
+
+back :: (a,b,c) -> c
+back (_,_,x) = x 
+
+getNeighbours :: (Eq e, Eq n) => (Graph g n e) => g -> n -> [n]
+getNeighbours g n = [i | i <- nodes g, i /= n, edge g (n, i) /= Nothing]
+
+relax :: (Num e, Ord e) => (n,n,(Inf e)) -> n -> (Inf e) -> (Inf e) -> (n,n,(Inf e))
+relax (to,_, INF) newFrom (NI fromW) (NI edgeW) = (to, newFrom, NI $ fromW + edgeW)
+relax (to, oldFrom, (NI oldWeight)) newFrom (NI fromW) (NI edgeW) 
+	| newWeight < oldWeight = (to, newFrom, NI newWeight)
+	| otherwise =  (to, oldFrom, NI oldWeight)
+		where newWeight = edgeW + fromW
+
+relaxNeighbours ::  (Graph g n e, Num e, Ord e, Eq n) => g -> n -> [(n,n, (Inf e))] -> [(n, n, (Inf e))]
+relaxNeighbours graph source stateList = 
+	map (\(to,from, weight) -> if elem to neighbours 
+		then relax (to, from, weight) source sourceW $ NI $ fromJust (edge graph (source, to))
+		else (to, from, weight))
+	stateList
+	where 
+		neighbours = getNeighbours graph source
+		sourceW = back $ fromJust (find (\(to,_, weight) -> to == source) stateList)
+
+dijkstraLoop :: (Graph g n e, Num e, Ord e, Eq n) => g -> [(n,n, (Inf e))] -> [n] -> [n] -> n -> [(n,n, (Inf e))]
+dijkstraLoop _ s _ [] _ = s
+dijkstraLoop graph stateList visited unvisited current = 
+	dijkstraLoop graph newState (current:visited) (delete (front smallest) unvisited) (front smallest)
+	where 
+		newState = relaxNeighbours graph current stateList
+		smallest = foldl accFunc (head unvisited, head unvisited, INF) newState 
+		accFunc (to', from', weight') (to, from, weight)  
+			| notElem to unvisited = (to', from', weight')
+			| weight > weight' = (to', from', weight')
+			| otherwise = (to, from, weight)
+
+getPath ::  (Eq n, Eq e) => [(n,n, (Inf e))] -> n -> n -> [n] -> Maybe [n]
+getPath state dest curr lst
+	| dest == curr = Just lst
+	| weight == INF = Nothing
+	| otherwise =  getPath state dest from (node:lst)
+		where 
+			node = front triple
+			from = center triple
+			weight = back triple
+			triple = fromJust $ find (\(node, from, weight) -> node == curr) state
+
+dijkstra :: (Num e, Eq e, Eq n, Ord e, Graph g n e) => g -> n -> n -> Maybe [n]
+dijkstra graph source end
+	| nodes graph == [] = Nothing
+	| otherwise = getPath (dijkstraLoop
+		graph
+		(map (\x -> if x == source then (x, x, NI 0) else (x, x, INF)) (nodes graph))
+		[]
+		(delete source (nodes graph))
+		source)
+	source end []
--- Project.hs
+++ Project.hs
@@ -1,3 +1,17 @@
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies,FlexibleInstances, FlexibleContexts #-}
 
+-- DEBUG
 import Graph.BinarySearchTree
+import Graph.Dijkstra
+
+
+t0 = foldl (\acc x -> insertNode acc x) (empty :: (BSTGraph Int Int)) [1..5]
+t1 = insertEdge t0 (1,2) 1
+t2 = insertEdge t1 (2,3) 1
+t3 = insertEdge t2 (3,4) 1
+t4 = insertEdge t3 (4,5) 1
+t5 = insertEdge t4 (1,5) 2
+
+x = t4
+y = t5
+-- END DEBUG
