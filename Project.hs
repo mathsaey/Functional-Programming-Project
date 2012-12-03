07428a042f44aefca8e68a14aa36efb5f202c6a8
Mathijs Saey <mathsaey@Greyflood.local>
2012-12-03 03:03:54 +0100

    Removed some debug code

--- Project.hs
+++ Project.hs
@@ -7,24 +7,6 @@ import Graph.Kernel
 import Graph.Layout
 import Graph.Dijkstra
 
--- DEBUG
-import Graph.BinarySearchTree
-
-
-
-t0 = foldl (\acc x -> insertNode acc x) (empty :: (BSTGraph Int Int)) [1..5]
-t1 = insertEdge t0 (1,2) 1
-t2 = insertEdge t1 (2,3) 1
-t3 = insertEdge t2 (3,4) 1
-t4 = insertEdge t3 (4,5) 1
-t5 = insertEdge t4 (1,5) 2
-
-x = t4
-y = t5
-
-gen = mkStdGen 32242342
--- END DEBUG
-
 nodeRadius = 0.5
 
 drawNode :: (n, Float, Float) -> String
