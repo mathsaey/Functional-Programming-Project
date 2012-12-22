5a68f62573da4f4fa51b7f9eb5fc46a08a0f7a5d
Mathijs Saey <mathsaey@Greyflood.local>
2012-12-22 15:23:34 +0100

    Moved the toPsP code to it's own file

---
+++ LatexDraw.hs
@@ -0,0 +1,42 @@
+-- LatexDraw.hs
+-- Mathijs Saey
+-- This module contains the LatexDraw implementation
+
+module LatexDraw (toPsP) where
+
+import Data.List
+import Data.Maybe
+
+import Text.Printf
+import System.Random
+
+import Graph.Kernel
+import Graph.Layout
+import Graph.Dijkstra
+
+nodeRadius = 0.5
+
+drawNode :: (n, Float, Float) -> String
+drawNode (_, x, y) = 
+	printf "\\pscircle[linecolor = black](%.2f,%.2f){%.2f}" x y nodeRadius
+
+drawEdge :: (n, Float, Float) -> (n, Float, Float) -> String -> String
+drawEdge (_, fromX, fromY) (_, toX, toY) c = 
+	printf ("\\psline[linecolor =" ++ c ++ "]{->}(%.2f,%.2f)(%.2f,%.2f)") fromX fromY toX toY
+
+toPsP :: (Num e, Graph g n e, RandomGen r, Ord e, Eq n) => g -> r -> n -> n -> String
+toPsP graph gen from to = nodes ++ pathedgeStr ++ noPathedgeStr where
+		layoutL = layout graph gen
+		nodes = foldl (\acc x -> acc ++ (drawNode x) ++ "\n") "" layoutL
+		path = dijkstra graph from to
+		pathFrom = if path /= Nothing then fromJust path else []
+		pathNodes = map (\x -> fromJust (find (\(n,_,_) -> n == x) layoutL)) pathFrom
+		pathEdges = fst $ foldl (\(ls, e) x -> if e == x then (ls, x) else ((e,x):ls, x)) ([], head pathNodes) pathNodes
+		pathedgeStr = foldl (\acc (a,b) -> acc ++ (drawEdge a b "red" ++ "\n")) "" pathEdges
+		alledges = 
+			foldl (\ls (n1,x,y) -> ls ++
+				(foldl (\ls (a,b,c) -> ((n1,x,y), (a,b,c)) : ls) 
+					[] $ (filter (\(n2,_,_) -> edge graph (n1,n2) /= Nothing) layoutL))) 
+			[] layoutL
+		noPathEdges = alledges \\ pathEdges
+		noPathedgeStr = foldl (\acc (a,b) -> acc ++ (drawEdge a b "black" ++ "\n")) "" noPathEdges
--- Project.hs
+++ Project.hs
@@ -1,10 +1,9 @@
 -- Layout.hs
 -- Mathijs Saey
--- This module contains the implementation of the toPsP function. 
+-- This module contains the top level of the project
 
-import Data.Maybe
 import Data.List
-import Text.Printf
+import Data.Maybe
 import System.Random
 
 import Graph.Kernel
@@ -13,6 +12,9 @@ import Graph.Dijkstra
 
 import Graph.AdjacencyMatrix
 import Graph.BinarySearchTree
+
+import Text.Printf
+import LatexDraw
 
 
 -- DEBUG
@@ -32,29 +34,3 @@ gen = mkStdGen 32242342
 -- DEBUG
 
 
-nodeRadius = 0.5
-
-drawNode :: (n, Float, Float) -> String
-drawNode (_, x, y) = 
-	printf "\\pscircle[linecolor = black](%.2f,%.2f){%.2f}" x y nodeRadius
-
-drawEdge :: (n, Float, Float) -> (n, Float, Float) -> String -> String
-drawEdge (_, fromX, fromY) (_, toX, toY) c = 
-	printf ("\\psline[linecolor =" ++ c ++ "]{->}(%.2f,%.2f)(%.2f,%.2f)") fromX fromY toX toY
-
-toPsP :: (Num e, Graph g n e, RandomGen r, Ord e, Eq n) => g -> r -> n -> n -> String
-toPsP graph gen from to = nodes ++ pathedgeStr ++ noPathedgeStr where
-		layoutL = layout graph gen
-		nodes = foldl (\acc x -> acc ++ (drawNode x) ++ "\n") "" layoutL
-		path = dijkstra graph from to
-		pathFrom = if path /= Nothing then fromJust path else []
-		pathNodes = map (\x -> fromJust (find (\(n,_,_) -> n == x) layoutL)) pathFrom
-		pathEdges = fst $ foldl (\(ls, e) x -> if e == x then (ls, x) else ((e,x):ls, x)) ([], head pathNodes) pathNodes
-		pathedgeStr = foldl (\acc (a,b) -> acc ++ (drawEdge a b "red" ++ "\n")) "" pathEdges
-		alledges = 
-			foldl (\ls (n1,x,y) -> ls ++
-				(foldl (\ls (a,b,c) -> ((n1,x,y), (a,b,c)) : ls) 
-					[] $ (filter (\(n2,_,_) -> edge graph (n1,n2) /= Nothing) layoutL))) 
-			[] layoutL
-		noPathEdges = alledges \\ pathEdges
-		noPathedgeStr = foldl (\acc (a,b) -> acc ++ (drawEdge a b "black" ++ "\n")) "" noPathEdges
